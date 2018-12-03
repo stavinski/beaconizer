23a80843d8694c9723e3005c9ed659436ec3a914
Mike Cromwell <stavinski_2000@yahoo.com>
2018-12-03 23:16:20 +0000

    refactoring to use proper threading & fixed wifi iface object

--- beaconizer.py
+++ beaconizer.py
@@ -3,7 +3,6 @@
 
 # Todo:
 #
-# Proper threading
 # Tweak timings with testing against different clients
 
 __description__ = "beaconizer"
@@ -12,17 +11,16 @@ __version__ = "1.0.0"
 __date__ = "2018/11/22"
 
 
-import threading
 import time
 import sys
 
+from threading import Thread
 from argparse import ArgumentParser, FileType
 from scapy.all import *
 from wifi import WifiInterface
 
 
 # globals
-probes = set()
 ssids = []
 args = {}
 
@@ -30,20 +28,27 @@ args = {}
 IFACE_DEFAULT = 'wlan0'
 CHANNEL_DEFAULT = 11
 
-
-def sniff_probe(pkt):
-  if pkt.haslayer(Dot11ProbeReq):
-    name=pkt[Dot11ProbeReq].info
-    if len(name) > 0 and not name in probes:
-      probes.add(name)
-      mac_src=pkt[Dot11].addr2
-      escaped_name=name.replace("'","''")      
-      print >> args.outfile, "'%s', %s" % (escaped_name, mac_src)
-      
-
-def sniff_probes():
-  sniff(prn=sniff_probe, filter="wlan type mgt subtype probe-req", store=False)
-
+"""handles sniffing for probes in a thread context 
+"""
+class ProbeSniffingThread(Thread):
+    
+    def __init__(self):
+        super(ProbeSniffingThread, self).__init__()        
+        self._probes = set()
+        self.daemon = True # we want to stop when parent thread stops
+    
+    def _packet_filter(self, pkt):
+      if pkt.haslayer(Dot11ProbeReq):
+        name = pkt[Dot11ProbeReq].info
+        if len(name) > 0 and not name in self._probes:
+          self._probes.add(name)
+          mac_src = pkt[Dot11].addr2
+          escaped_name = name.replace("'","''")      
+          print >> args.outfile, "'%s', %s" % (escaped_name, mac_src)
+        
+    def run(self):
+        sniff(prn=self._packet_filter, filter="wlan type mgt subtype probe-req", store=False)
+                
 
 def send_beacons():
 
@@ -51,7 +56,7 @@ def send_beacons():
     for ssid in ssids:
 
       dot11 = Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff',
-      addr2=str(RandMAC()), addr3=str(RandMAC()))
+      addr2 = str(RandMAC()), addr3=str(RandMAC()))
       beacon = Dot11Beacon(cap='ESS')
       essid = Dot11Elt(ID='SSID', info=ssid, len=len(ssid))
 
@@ -68,17 +73,14 @@ def main():
 
   # wifi setup
   wifi_iface = WifiInterface(args.iface)
-  wifi_iface.set_monitor()
   wifi_iface.set_channel(args.channel)
-
+  wifi_iface.set_monitor()
+  
   try:
-    
-    for ssid in args.ssids:
-      ssids.append(ssid.rstrip('\n'))
+    ssids = [ssid.rstrip("\n") for ssid in args.ssids]    
 
-    t_probes=threading.Thread(target=sniff_probes)
-    t_probes.daemon = True
-    t_probes.start()
+    probe_thread = ProbeSniffingThread()
+    probe_thread.start()
     send_beacons()
 
   except KeyboardInterrupt:
